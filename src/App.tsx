f67ad403e61706fbbc729cafb8281d160d3ea9b7
AzureAD\ZainalAbidin <zainal.abidin@biposervice.com>
2024-06-21 17:23:28 +0700

    setup config alias path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect } from "react";
+
 import "./App.css";
+
+import { Banner, Header } from "@/components";
 
 import ExperienceRecord from "./interface/experience";
 
@@ -10,46 +11,41 @@ interface ApiResponse {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [dataExperiences, setDataExperiences] = useState<ApiResponse>({
-    records: [],
-  });
+  // const [dataExperiences, setDataExperiences] = useState<ApiResponse>({
+  //   records: [],
+  // });
 
-  const fetchExperiences = async () => {
-    try {
-      const response = await fetch(
-        "https://api.airtable.com/v0/app0bJmhrbFR4Stk4/experiences",
-        {
-          headers: {
-            Authorization:
-              "Bearer patQg4hrbDvUXq1bu.5d5001a9b110cc709a192df23844aeee6a934989e72c389f6390bd13d24303a3",
-          },
-        }
-      );
+  // const fetchExperiences = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://api.airtable.com/v0/app0bJmhrbFR4Stk4/experiences",
+  //       {
+  //         headers: {
+  //           Authorization:
+  //             "Bearer patQg4hrbDvUXq1bu.5d5001a9b110cc709a192df23844aeee6a934989e72c389f6390bd13d24303a3",
+  //         },
+  //       }
+  //     );
 
-      const data: ApiResponse = await response.json();
+  //     const data: ApiResponse = await response.json();
 
-      setDataExperiences(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     setDataExperiences(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   useEffect(() => {
-    fetchExperiences();
+    // fetchExperiences();
   }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <Banner />
       </div>
-      {dataExperiences.records.map((item) => (
+      {/* {dataExperiences.records.map((item) => (
         <p>{item.fields.title}</p>
       ))}
       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
@@ -64,7 +60,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   );
 }
