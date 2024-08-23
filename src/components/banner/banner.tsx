8b433aad204dca422e313a30d3c93dd8bb74c140
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-08-23 10:33:32 +0700

    feat: change color icon when hover

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -3,11 +3,21 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithubSquare } from "react-icons/fa";
 
 import { useTheme } from "../theme/theme-provider";
+import { useState } from "react";
+import { colors } from "@/assets/colors";
 
 const Banner = () => {
   const { theme } = useTheme();
+  const [isHovered, setIsHovered] = useState("");
 
   const color = theme == "light" ? "#2f4858" : "#ffffff";
+
+  const iconStyles = (color: string, secondColor: string, type: string) => {
+    return {
+      color: isHovered === type ? secondColor : color,
+      transition: "color 0.6s ease",
+    };
+  };
 
   return (
     <section className="flex w-full min-h-screen mt-10 items-center py-20 dark:bg-gray-900">
@@ -19,14 +29,38 @@ const Banner = () => {
           A Software Engineer
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-          <a href="https://linkedin.com/in/zainal-abidin-08/" target="_blank">
-            <FaLinkedin size={44} color={color} />
+          <a
+            href="https://linkedin.com/in/zainal-abidin-08/"
+            target="_blank"
+            onMouseEnter={() => setIsHovered("linkedin")}
+            onMouseLeave={() => setIsHovered("")}
+          >
+            <FaLinkedin
+              size={44}
+              style={iconStyles(color, colors.third, "linkedin")}
+            />
           </a>
-          <a href="https://twitter.com/zaabdn" className="mx-1">
-            <FaSquareXTwitter size={44} color={color} />
+          <a
+            href="https://twitter.com/zaabdn"
+            className="mx-1"
+            onMouseEnter={() => setIsHovered("twitter")}
+            onMouseLeave={() => setIsHovered("")}
+          >
+            <FaSquareXTwitter
+              size={44}
+              style={iconStyles(color, colors.third, "twitter")}
+            />
           </a>
-          <a href="https://github.com/zaabdn" target="_blank">
-            <FaGithubSquare size={44} color={color} />
+          <a
+            href="https://github.com/zaabdn"
+            target="_blank"
+            onMouseEnter={() => setIsHovered("github")}
+            onMouseLeave={() => setIsHovered("")}
+          >
+            <FaGithubSquare
+              size={44}
+              style={iconStyles(color, colors.third, "github")}
+            />
           </a>
         </div>
       </div>
