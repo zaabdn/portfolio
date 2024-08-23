7c8bf0c1a5569abc3b2dde85b155a928ba8abba9
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-08-23 14:30:05 +0700

    fix footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <footer className=" bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <footer className="rounded-lg shadow m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="" className="hover:underline">
             Zainal Abidin
           </a>
         </span>
