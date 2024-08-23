1d30265b2cff6d924dd971f16952c1628d37be51
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-08-23 14:29:43 +0700

    feat: add dummy image and link to

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,31 +1,42 @@
 import { dataProjects } from "@/data/projects";
 import { Separator } from "../ui/separator";
+import { Element } from "react-scroll";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
-    <section className="w-full pt-20">
+    <Element name="projects" className="w-full pt-20">
       <div className="flex flex-row justify-between flex-wrap mx-20">
         {dataProjects.map((item) => (
-          <div className="mb-16 ml-4 w-1/5">
-            <div className="space-y-1">
-              <h4 className="text-sm font-medium leading-none">{item.title}</h4>
+          <Link
+            to=""
+            // key={item.title}
+            className="mb-16 ml-4 w-1/5 hover:scale-x-105 hover:scale-y-105"
+            key={item.title}
+          >
+            <img
+              src="https://placehold.co/500x300?text=Hello+World"
+              className="rounded-sm"
+            />
+            <div className="space-y-1 text-left mt-5">
+              <h4 className="text-xl font-medium leading-none">{item.title}</h4>
               <p className="text-sm text-muted-foreground">
                 {item.description}
               </p>
             </div>
             <Separator className="my-4" />
-            <div className="flex h-5 items-center space-x-4 text-sm">
-              {item.stack.split(",").map((o) => (
-                <div>
+            <div className="flex h-5 items-center space-x-4 -mt-3 text-sm">
+              {item.stack.split(",").map((o, i) => (
+                <div key={i}>
                   <div>{o}</div>
                   <Separator orientation="vertical" />
                 </div>
               ))}
             </div>
-          </div>
+          </Link>
         ))}
       </div>
-    </section>
+    </Element>
   );
 };
 
