6c97483658472ec6d9a5901dd4e8359d6302a2b8
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-09-24 10:29:42 +0700

    Revert "add career section"
    This reverts commit 1122f3f1a4491fa227e47acf01962815ff3a925a.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as Header } from "./header/header";
 export { default as Banner } from "./banner/banner";
+export { default as DarkModeToggle } from "./darkMode/darkMode";
 export { default as Footer } from "./footer/footer";
 export { default as Career } from "./career/career";
 export { default as DownloadCV } from "./download/downloadCV";
